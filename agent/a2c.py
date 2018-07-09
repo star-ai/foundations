5ba784c5a43ce4d02748b7bb5880932f675f8e06
whathelll <whathell@gmail.com>
2018-07-09 23:28:33 +1000

    initial attempt: agent selecting moves on probability dist of brain

--- agent/a2c.py
+++ agent/a2c.py
@@ -2,11 +2,19 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+from brain.screen_only import ScreenSelectAndMoveBrain
+from memory.memory import ReplayMemory, Transition
 
 from pysc2.agents import base_agent
 from pysc2.lib import actions
 from pysc2.lib import features
+
+from util.tf_helpers import TFHelper
+
+tfe.enable_eager_execution()
 
 _PLAYER_SELF = features.PlayerRelative.SELF
 _PLAYER_NEUTRAL = features.PlayerRelative.NEUTRAL  # beacon/minerals
@@ -25,16 +33,60 @@ class A2CMoveToBeacon(base_agent.BaseAgent):
   """A2C agent for move to beacon"""
   def __init__(self):
     super(A2CMoveToBeacon, self).__init__()
+    self.action_optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+    self.value_optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+    self.brain = ScreenSelectAndMoveBrain()
+    self.memory = ReplayMemory(capacity=10)
 
+    self.s_0 = None
+    self.a_0 = None
+    self.s_1 = None
+    self.r_1 = None
+    self.done = None
 
   def step(self, obs):
     super(A2CMoveToBeacon, self).step(obs)
+
+    self.s_1 = obs.observation.feature_screen.player_relative  #make it simple for now
+    self.s_1 = np.expand_dims(self.s_1, axis=2)
+
+    self.r_1 = obs.reward
+    self.done = obs.reward == 1
+
+    if self.s_0 is not None and self.a_0 is not None:
+      a = Transition(self.s_0, self.a_0, self.s_1, self.r_1, self.done)
+      self.memory.push(a)
+      self.s_0 = self.s_1
+      self.a_0 = None
+
+    if self.s_0 is None:
+      self.s_0 = self.s_1
+
     if FUNCTIONS.Move_screen.id in obs.observation.available_actions:
       player_relative = obs.observation.feature_screen.player_relative
       beacon = _xy_locs(player_relative == _PLAYER_NEUTRAL)
+      # print("reward:", obs.reward)
       if not beacon:
         return FUNCTIONS.no_op()
-      beacon_center = numpy.mean(beacon, axis=0).round()
-      return FUNCTIONS.Move_screen("now", beacon_center)
+
+      action = self.get_action(self.s_0)
+      print("Move action:", action)
+      self.a_0 = action
+      # beacon_center = np.mean(beacon, axis=0).round()
+      return FUNCTIONS.Move_screen("now", action)
     else:
       return FUNCTIONS.select_army("select")
+
+  def get_best_action(self, s_0):
+    s_0 = np.expand_dims(s_0, axis=0) # convert to batch
+    s_0 = tf.constant(s_0, dtype=tf.float32)
+    p, v = self.brain(s_0)
+    coords = TFHelper.argmax_2D(p)
+    return coords.numpy().squeeze()
+
+  def get_action(self, s_0):
+    s_0 = np.expand_dims(s_0, axis=0) # convert to batch
+    s_0 = tf.constant(s_0, dtype=tf.float32)
+    p, v = self.brain(s_0)
+    coords = TFHelper.choose_2D(p)
+    return coords
--- agent/test/test_a2c.py
+++ agent/test/test_a2c.py
@@ -2,7 +2,7 @@ import unittest
 
 
 from agent.a2c import A2CMoveToBeacon
-
+import numpy as np
 
 
 class TestA2CMoveToBeacon(unittest.TestCase):
@@ -10,11 +10,24 @@ class TestA2CMoveToBeacon(unittest.TestCase):
     super().__init__(*args, **kwargs)
 
   def setUp(self):
-    pass
+    self.agent = A2CMoveToBeacon()
 
-  def test_split(self):
-    self.assertTrue(1 == 1, "doh")
 
+  @unittest.skip("manual test: can't seriously test this one cus the model gives random results")
+  def test_select_best_action(self):
+    s = np.zeros((5, 5, 1))
+    s[0][3][0] = 1.0
+    a = self.agent.get_best_action(s)
+
+    print(a)
+
+  @unittest.skip("manual test: can't seriously test this one cus the model gives random results")
+  def test_select_action(self):
+    s = np.zeros((5, 5, 1))
+    s[0][3][0] = 1.0
+    a = self.agent.get_action(s)
+
+    print(a)
 
 if __name__ == "__main__":
   unittest.main()
